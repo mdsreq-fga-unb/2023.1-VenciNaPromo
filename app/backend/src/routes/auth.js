6c34a8ed10ed25199b95cdee748503799751120c
freitasc <caina2000@gmail.com>
2023-06-17 18:31:21 -0300

    muda dependencia do bcrypt

--- app/backend/src/routes/auth.js
+++ app/backend/src/routes/auth.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');

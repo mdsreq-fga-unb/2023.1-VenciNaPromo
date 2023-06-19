f1ee1bc8dfefbde21b9be9f20fcb82eddd6a994a
Charles <charles.serafim.morais@gmail.com>
2023-06-19 14:20:06 -0300

    adicionando produtos na lista de produtos
    Co-authored-by: Gabrielly Assunção <gabriellyassuncaorodrigues@gmail.com>

--- app/frontend/src/components/ProductInList.jsx
+++ app/frontend/src/components/ProductInList.jsx
@@ -3,6 +3,8 @@ import ProductDetail from '../components/ProductDetail';
 
 import '../styles/ProductInList.css';
 import '../styles/ProductDetail.css';
+import '../styles/ShoppingList.css';
+import { productList } from '../services/products';
 
 const ProductInList = (product) => {
     const [showPopup, setShowPopup] = useState(false);
@@ -11,17 +13,19 @@ const ProductInList = (product) => {
         setShowPopup(!showPopup);
     };
 
+    console.log(product);
+
     return (
         <div className="product-in-list" onClick={togglePopup}>
             <div className="product-in-list__image">
-                <img src={product.product_image} alt="Imagem do Produto 1" />
+                <img src={product.product.product_image} alt="Imagem do Produto 1" />
             </div>
             <div className="product-in-list__info">
                 <div className="product-in-list__info__name">
-                    <h3>Product Name</h3>
+                    <h3>{product.product.product_name}</h3>
                 </div>
                 <div className="product-in-list__info__price">
-                    <h3>R$ 0,00</h3>
+                    <h3>{product.product.product_price}</h3>
                 </div>
                 <button className="product-in-list-button" onClick={togglePopup}>
                     <span className="plus-icon"></span>
@@ -32,14 +36,14 @@ const ProductInList = (product) => {
               <div className="popup-content">
                 <div className="product-detail-container">
                     <div className="product-detail">
-                        <img src="https://www.omanahprodutosnaturais.com.br/image/cache/catalog/feijao%20vermelho-800x800.jpg" alt="Imagem do Produto 1" />
-                        <div className="product-detail-title">Feijãozinho</div>
+                        <img src={product.product.product_image} alt="Imagem do Produto 1" />
+                        <div className="product-detail-title">{product.product.product_name}</div>
                         <div className="product-detail-text-box">
-                            <div className="product-detail-store">Loja tal - Águas Claras</div>
-                            <div className="product-detail-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elementum fringilla lectus at rutrum. Quisque maximus metus vitae risus rutrum, ut ullamcorper ipsum aliquam. Nunc ut velit dignissim, consequat mauris ut, tristique purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam semper vestibulum fermentum. Aenean in fermentum enim. Nullam et pharetra ligula. Vivamus eu libero ut felis ullamcorper malesuada. Nullam ullamcorper odio et eleifend tristique.</div>
-                            <div className="product-detail-quantity">Quantidade disponível: 5</div>
+                            <div className="product-detail-store">Empório Jardim Botânico</div>
+                            <div className="product-detail-description">{product.product.product_description}</div>
+                            <div className="product-detail-quantity">Quantidade disponível: {product.product.product_quantity}</div>
                         </div>
-                        <div className="product-detail-price">R$ 149,99</div>
+                        <div className="product-detail-price">R$ {product.product.product_price}</div>
                         <div className="product-detail-button">Adicionar</div>
                     </div>
                   </div>

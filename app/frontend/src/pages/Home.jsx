19d6838d07f4984b70804a1cc3bc4e334f9d24fd
freitasc <caina2000@gmail.com>
2023-06-19 16:34:47 -0300

    termina comeco da home

--- app/frontend/src/pages/Home.jsx
+++ app/frontend/src/pages/Home.jsx
@@ -4,29 +4,29 @@ import { ErrorMessage, Formik, Form, Field } from "formik";
 import { login, register } from "../services/auth";
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
-import '../styles/Login.css';
+import '../styles/Home.css';
+import { logout } from "../services/auth";
 
 function Home() {
 
 
   return (
-    <div className="home-container">
+    <div className="container">
       <Sidebar />
       <Header />
-
-      <div className='image-container'>
-        <img src="https://i.imgur.com/3Z1Qq4P.png" alt="logo" />
-      </div>
-      <div className="home-content">
-        <div className="home-text">
-          <h1>Encontre as melhores promoções da sua cidade!</h1>
-          <p>Com o Venci na Promo você pode encontrar as melhores promoções da sua cidade, e ainda ganhar cupons para trocar por produtos!</p>
+    
+      <div className="home-container">
+        <div className='image-container'>
+          <img src="https://i.imgur.com/3Z1Qq4P.png" alt="logo" />
         </div>
-        <div className="home-buttons">
-          <button className="home-button">Quero me cadastrar</button>
-        </div>
-        <div className="home-info">
-          <h2>Como funciona?</h2>
+        <div className="home-content">
+          <div className="home-text">
+            <p>Texto resumo falando sobre o projeto e explicando o problema e a causa.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget sodales justo.</p>
+          </div>
+          <div className="home-info">
+            <button className="app-button" onClick={logout}>Ir para a aplicação</button>
+          </div>
         </div>
       </div>
     </div>

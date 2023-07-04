258ff2f1a784e883868148130a5464d25f4d0af0
Charles Serafim Morais <charles.serafim.morais@gmail.com>
2023-07-04 02:17:22 -0300

    corrigindo estilos de componentes

--- app/frontend/src/components/Header.jsx
+++ app/frontend/src/components/Header.jsx
@@ -3,18 +3,17 @@ import '../styles/Header.css';
 
 function Header() {
   return (
-    <header>
+    <div className="header">
       <div className="header-left">
         <span>VenciNaPromo</span>
       </div>
       <div className="header-right">
         <ul>
           <li><a href="/home">Home</a></li>
-          <li><a href="/">Comida</a></li>
-          <li><a href="/">Casa</a></li>
+          <li><a href="/">Mercado</a></li>
         </ul>
       </div>
-    </header>
+    </div>
   );
 }
 

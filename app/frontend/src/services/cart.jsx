888130d9681fd5089d57ccd6cfc2ab8e3ed28413
Charles Serafim Morais <charles.serafim.morais@gmail.com>
2023-07-19 18:26:57 -0300

    mudança decremento produto

--- app/frontend/src/services/cart.jsx
+++ app/frontend/src/services/cart.jsx
@@ -10,7 +10,7 @@ export const saveProductInCart = (props) => {
     localStorage.setItem('cart', JSON.stringify(cart));
 
     // console.log(props.quantity);
-    // console.log(cart);
+    console.log(cart);
 
     window.location.reload();
 };
@@ -25,7 +25,7 @@ export const removeProductFromCart = (props) => {
         return product._id !== props._id;
     }
     );
-    localStorage.setItem('cart', JSON.stringify(cart)) ;
+    localStorage.setItem('cart', JSON.stringify(cart));
     window.location.reload();
 }
 
@@ -76,9 +76,9 @@ export const getCheckoutTotal = () => checkoutData;
 
 const BASE_URL = process.env.REACT_APP_API_URL;
 
-export const checkout = async () => {
+export const checkout = async (cart) => {
     //sends cart to backend and returns a coupon
-    let cart = JSON.parse(localStorage.getItem('cart'));
+    // let cart = JSON.parse(localStorage.getItem('cart'));
     if (cart) {
         const token = localStorage.getItem('token');
         await fetch(BASE_URL + "/order/finish_order/", {
@@ -95,12 +95,46 @@ export const checkout = async () => {
             .then(response => { return response.json() })
             .then(data => {
                 checkoutData = data.order;
+
+                // console.log(cart);
+
+                cart.forEach(product => {
+                    updateProductQuantity(product._id, product.product_quantity - product.quantity);
+                });
+
                 return checkoutData;
             });
     }
     return null;
 }
 
+
+export const updateProductQuantity = async (productId, newQuantity) => {
+    try {
+        console.log(productId);
+        console.log(newQuantity);
+
+        const response = await fetch(BASE_URL + `/product_list/update_product/${productId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('token'),
+            },
+            body: JSON.stringify({ product_quantity: newQuantity })
+        });
+
+        if (!response.ok) {
+            throw new Error('Error updating product quantity');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+        throw new Error('Error updating product quantity');
+    }
+};
 
 let ordersData = null;
 
